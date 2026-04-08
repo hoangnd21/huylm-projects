15c74b4cf1ee2f37c7a5f4975e6b90e9a4e5c2aa
huy <chronosdelta01@gmail.com>
2026-04-08 16:56:37 +0700

    Multi-Page Restaurant Site

---
+++ phase4-dish-survey/js/header.js
@@ -0,0 +1,48 @@
+const path = location.pathname.includes("pages")
+  ? "../components/header.html"
+  : "components/header.html";
+
+fetch(path)
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("header-container").innerHTML = data;
+
+    fixLinks();
+    initTheme();   
+  });
+
+
+function fixLinks() {
+  const links = document.querySelectorAll("[data-link]");
+
+  links.forEach(link => {
+    const target = link.getAttribute("data-link");
+
+    if (location.pathname.includes("pages")) {
+      link.href = "../" + target;
+    } else {
+      link.href = target;
+    }
+  });
+}
+
+function initTheme() {
+  const btn = document.getElementById("themeToggle");
+
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    btn.innerText = "☀️";
+  }
+
+  btn.onclick = () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+      btn.innerText = "☀️";
+    } else {
+      localStorage.setItem("theme", "light");
+      btn.innerText = "🌙";
+    }
+  };
+}
